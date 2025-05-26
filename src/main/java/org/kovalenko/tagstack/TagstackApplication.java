83cf755778a882c9ec88d731f85c205a14464968
merrymike-noname <kovalenko.mykhailo1@lll.kpi.ua>
2025-05-26 13:12:38 +0300

    Initial commit

---
+++ src/main/java/org/kovalenko/tagstack/TagstackApplication.java
@@ -0,0 +1,13 @@
+package org.kovalenko.tagstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kovalenko/tagstack/TagstackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kovalenko.tagstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
